b9e270dc8de93e43f4975f5052cea77276f8c79b
nikit696 <nikitalazarchyk@MacBook-Pro-Nikita.local>
2022-01-06 20:06:44 +0300

    to 40

---
+++ 39.js
@@ -0,0 +1,9 @@
+const myNoun = "dog"
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "It was really " + myNoun + ", and we " + myVerb + " ourselves " + myAdverb + ".";; // Change this line
+// Only change code above this line
+console.log(wordBlanks);
